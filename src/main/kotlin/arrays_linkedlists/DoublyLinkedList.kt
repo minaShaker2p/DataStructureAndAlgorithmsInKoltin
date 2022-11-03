1064970f260e3b463b09cd451ca87027aa00b1a7
Mina Rezkalla <minashakeralfy@gmail.com>
2022-11-03 18:47:48 +0200

    add isEmpty & size & first and last functions

--- src/main/kotlin/arrays_linkedlists/DoublyLinkedList.kt
+++ src/main/kotlin/arrays_linkedlists/DoublyLinkedList.kt
@@ -1,7 +1,38 @@
 package arrays_linkedlists
 
-class DoublyLinkedList<T> {
+class DoublyLinkedList<T>() {
     // node data class to represent the doubly linked list node
-    data class Node<T>(val element:T, val prev:Node<T> , val next:Node<T>)
+    data class Node<T>(var element: T?, var prev: Node<T>?, var next: Node<T>?)
+
+    // instance variables  of the doublyLinkedList
+    private var header: Node<T> = Node<T>(null, null, null)
+    private var trailer: Node<T> = Node<T>(null, header, null)
+
+    init {
+        header.next = trailer
+    }
+
+    /** Returns the number of element in the linked list */
+    fun size() = size
+    /** Tests whether the linked list is empty */
+    fun isEmpty()= size == 0
+
+    /** Returns (but does not remove) the first element in the linked list*/
+    fun first():T?{
+        if(isEmpty())
+            return null
+        return header.next?.element
+    }
+
+
+    /** Returns (but does not remove) the last element in the linked list*/
+    fun last():T?{
+        if(isEmpty())
+            return null
+        return trailer.prev?.element
+    }
+
+    private var size = 0
+
 
 }
